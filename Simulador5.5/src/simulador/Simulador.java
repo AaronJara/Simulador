144328b06638536a4854fc6f2d56e30bbd24eed6
AaronJara <aaronjamo96@live.com>
2016-03-13 22:29:46 -0600

    Avance
    1.0

---
+++ Simulador5.5/src/simulador/Simulador.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simulador; //Nombre del Paquete de la Clase
+/**
+ * @author Andrés J. Jiménez Leandro y Oscar F. Fonseca Salicetti
+ * @author Arbay Fernández Solano y Aaron Jara Molina
+ */
+public class Simulador { //Nombre del Main del Programa
+
+    public static void main(String[] args) { //Inicio y Argumentación del Main
+        //Proceso de Definición de las Instancias: 
+        Menu menu = new Menu();
+        //Llamo al Metodo menu de mi Clase Menu.
+        menu.Menu();
+    } //Fin del Main
+} //Fin del Programa en JaVa
